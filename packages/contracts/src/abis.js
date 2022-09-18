fdb0c4f805beb0cfd52448a7c7e0a04c2fa96df3
Ruben Santana <rubensantana@outlook.com>
2022-09-17 20:06:39 -0400

    Initial commit

---
+++ packages/contracts/src/abis.js
@@ -0,0 +1,7 @@
+import UBIburnerAbi from "./abis/UBIburner-goerli.json";
+
+const abis = {
+  UBIburner: UBIburnerAbi,
+};
+
+export default abis;
---
+++ packages/contracts/src/addresses.js
@@ -0,0 +1,11 @@
+// This address points to a contract deployed on Ethereum Mainnet,
+// Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
+//kovan="0x3944399B0B0184FcC55b3912d8aA5930b9E9b5AA"
+//kovan2 = "0xCFb99C3ac72677a61708892918dC104456fDA067";
+//goerli = "0xbAA2e3e008FF800e05Ccb9FE6aFD7BE3365C7cbe";
+const addresses = {
+  // contractUBIburner: "0x481B24Ed5feAcB37e282729b9815e27529Cf9ae2", //mainnet
+  contractUBIburner: "0xbAA2e3e008FF800e05Ccb9FE6aFD7BE3365C7cbe",
+};
+
+export default addresses;
